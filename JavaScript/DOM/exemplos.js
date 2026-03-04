6982cc86af88b5b982a8da7c47c5133430305df3
Beatriz Nobrega <beatriz.nobrega@aluno.senai.br>
2026-03-04 12:19:36 -0300

    aula DOM

--- JavaScript/DOM/exemplos.js
+++ JavaScript/DOM/exemplos.js
@@ -5,3 +5,12 @@ let url = document.getElementById("link").getAttribute("href");
 console.log(url); //https://br.pinterest.com/
 document.getElementById("caixa").style.backgroundColor = "lightblue";
 document.getElementById("alerta").classList.add("destaque");
+document.getElementById("alert").classList.remove("oculto");
+let novoParagrafo = document.createElement("p");
+novoParagrafo.textContent = "Este elemento foi criado pelo JavaScript.";
+document.getElementById("container").appendChild(novoParagrafo);
+let novoItem = document.createElement("li");
+novoItem.textContent = "Item novo";
+document.getElementById("lista").appendChild(novoItem);
+let item = document.getElementById("item1");
+document.getElementById("list").removeChild(item);
